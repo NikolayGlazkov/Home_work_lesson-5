3f2f6120c279d4683e945d5fd6e0529af7877ee3
NikolayGlazkov <gn9377419582@gmail.com>
2023-02-11 11:48:25 +0600

    task end

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("ввод b1: ");
+int b1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ввод k1: ");
+int k1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ввод b2: ");
+int b2 = int.Parse(Console.ReadLine());
+Console.WriteLine("ввод k2: ");
+int k2 = int.Parse(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine("Линии параллельны");
+
+    if (b1 == b2)
+    {
+        Console.WriteLine("Линии совпадают");
+    }
+    else
+    {
+        Console.WriteLine("Линии не пересекаются");
+    }
+}
+else
+{
+    float x = (b2 - b1) / (k1 - k2);
+    float y = k1 * x + b1;
+
+    Console.WriteLine("Прямые пересекаются в точке (" + x + "," + y + ")");
+}
